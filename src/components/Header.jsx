20891f79f79b83890f324c75f899d8e81799412f
jsonsfather <94735082+jsonsfather@users.noreply.github.com>
2021-12-26 01:05:36 +0330

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+return<header>
+  <h1>My Goodness</h1>
+  </header>
+
+}
+export default Header;
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Note() {
+  return (
+<div>
+  <h1>This is title</h1>
+  <p>This is content</p>
+  </div>
+  );
+}
+export default Note;
